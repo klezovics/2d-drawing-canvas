3052b41e837e0a2756bafcf4745b60eac8220ee6
klezo <klezo@LAPTOP-10L1CBFF>
2018-11-08 09:08:19 +0000

    Added a test for 5x5 test creation

--- stm-challenge/src/test/java/com/klezovich/stm_challenge/AppTest.java
+++ stm-challenge/src/test/java/com/klezovich/stm_challenge/AppTest.java
@@ -9,41 +9,39 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-	
-	
-	
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    
-    
-    public void test3by3gridCreation() {
-    	
-    	if(!CanvasTester.test( new File( Property.testDir+"canvas 3x3")))
-    		fail();
-    }
-    
-    public void testWrong3by3GridCreation() {
-    	if(CanvasTester.test( new File( Property.testDir+"canvas 3x3 wrong")))
-    		fail();
-    }
-    
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	public void test3by3gridCreation() {
+
+		if (!CanvasTester.test(new File(Property.testDir + "canvas 3x3")))
+			fail();
+	}
+
+	public void testWrong3by3GridCreation() {
+		if (CanvasTester.test(new File(Property.testDir + "canvas 3x3 wrong")))
+			fail();
+	}
+
+	public void test5by5gridCreation() {
+
+		if (!CanvasTester.test(new File(Property.testDir + "canvas 5x5")))
+			fail();
+	}
+
 }
